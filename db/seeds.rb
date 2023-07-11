3e8fddb3ce4592a22254833a8b2b52b9b43f7f7e
kiboma <omayioben@gmail.com>
2023-07-11 08:21:22 +0300

    Add data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Animal.destroy_all
+
+Animal.create!([
+    {name: "Dogs", description: "Do you have a pet dog? If so, you’re not alone. In the United States alone, it’s estimated that over 69 million households have a pet dog."},
+    {name: "Cats", description: "Domestic cats, also called house cats, are felines that are kept as indoor pets. A cat is habitually nocturnal (active at night) and has a good sense of smell. It tends to be lazy throughout the day. Cats are known for their independent nature and their ability to hunt rodents and other small animals."},
+    {name: "Rabbits", description: "A domesticated rabbit is supposed to have a lifespan of 8-12 years and is highly appreciated for its sense of hearing. Rabbits tend to get lonely when kept alone without any companion since they are social animals."},
+    {name: "Cows", description: "Cow as an animal is often adored for its calm nature. They are raised for their milk which is very nutritious and healthy. Cows are vegetarian and depend on short herbs, various kinds of grass, and some tree leaves for their food. Cows are female and spend 10 hours on average relaxing."},
+    {name: "Pigs", description: "Pigs are generally raised and bred for their meat. They are known for their good sense of direction and sharp memory. Pigs require a lot of space to roam and explore. They adore the company of their friends and families even while sleeping, and the mother pigs do sing for their newborns just as humans sing lullabies."},
+    {name: "Buffaloes", description: "Buffaloes are often used as working animals, as they are very strong and can be used for tasks such as ploughing fields or pulling carts."},
+    {name: "Sheep", description: "Sheep are raised for their meat and wool. It is the primary product of sheep and can be used to make clothing, blankets, and other textile items."},
+    {name: "Goats", description: "Goats are a type of livestock domestic animal that are raised for their meat consumption as well as for their milk. Just like the sheep, goats are extremely social and always need a herd to be with. Goats are appreciated for their good mountaineering skills, as they climb very effortlessly."},
+    {name: "Oxen", description: "The primary use of oxen is to plow fields in village areas across the world. They are known for their strength and stamina while plowing the fields."},
+    {name: "Horse", description: "A horse is usually raised for transportation purposes, generally signifying royalty and class. Rich people keep horses as a status symbol, and grooms at weddings also use horses to travel to the bride’s house on the wedding day.
+
+    A popular game of Polo is played while riding a horse or pony. They are known for their quick reflexes and intelligence. Also, they are social creatures. Horses use their mouth for breathing and can sleep even while standing."},
+    {name: "Camel", description: "Camels are found in desert areas and are used for transportation in hot desert areas full of sand. They can survive for weeks or even months on the 80 pounds of fat that they store in their humps. They can shut their nostrils at a sandstorm. Camels can easily sit in the scorching sand because of the thick pads of skin on their knees and chest. They can drink up to 40 gallons of water in a go."},
+    {name: "Donkey", description: "A donkey is usually raised to carry loads from one place to another. They have an average lifespan of 40-50 years. Contradicting the popular myth, donkeys are quite intelligent and strong.
+
+    They are believed to have an incredible memory, but they are stubborn in nature. They are social animals and hate being left alone. Their fur is not waterproof and can cause them health issues if left out in the rain for a prolonged period."},
+    {name: "Mule", description: "Mules are used to carry loads, mostly sand and construction stuff. They are the offspring of a female horse and a male donkey. Their skin is better resistant to sun and rain in comparison to both horses and donkeys. They are known to be sterile. They are less stubborn in comparison to a donkey. Just like donkeys, mules can live up to a lifespan of 50 years. And they do kick sideways when someone pisses them off."},
+    {name: "Chickens", description: "A chicken is a bird. Chickens are one of the most popular domesticated species in the world. They are usually raised for their eggs, although people also eat chicken meat. Each year, billions of chickens are raised for meat and eggs."},
+    {name: "Ducks", description: "Ducks are aquatic birds, mostly smaller than swans and geese, and may be found in freshwater and seawater. They are often kept as livestock on farms because they are relatively easy to care for and provide a valuable source of meat, eggs, and feathers. While ducks can be food sources, they can also be simply kept as pets. Less common domestic birds include parrots, doves, and quail. These birds are usually kept as pets."},
+    {name: "Pet Tortoise", description: "A pet tortoise can make a great animal companion. They are usually very low maintenance and can live for a long time. Tortoises can live in most climates and are ancient, with an incredible lifespan (up to 150 years). It is a difficult task to figure out a tortoise’s gender. Their shell is made up of bones and is quite complex. They smell with the help of their throats."},
+    {name: "Hedgehogs", description: "Hedgehogs are becoming increasingly popular pets, and it’s not hard to see why! They are adorable, relatively low-maintenance, and relatively quiet. Hedgehogs are omnivores, so their diet should consist of both plant and animal matter.
+
+    Many countries do not allow to import of hedgehogs (such as Australia), and some require a permit (such as the state of New Jersey) to own a hedgehog as a pet.
+    
+    They sometimes become slightly allergic to their surroundings and would have pink dots on their body as an allergic reaction. It is mainly kept in cages and can show signs of depression when not provided the range of distance to cover. They feast on insects. In a household, they can eat cooked chicken, pork, and sometimes even a small number of fruits and vegetables."},
+    {name: "Raccoons", description: "Raccoons are exotic pets, and many countries, including Japan, the UK, and even India, have banned the import of raccoons as pets. They are known for their high speeds and alertness."},
+    {name: "Mini Pigs", description: "Mini pigs are favorite pets in Mexico and are treated as a status symbol. They usually have poor eyesight. In various parts of the USA, laws are different for different locations for owning a mini pig. They provide a valuable contribution to various types of animal-assisted therapy."},
+    {name: "Flying Squirrels", description: "Flying squirrels are kept as pets in some states of the USA. They usually mate in late winter, and the newborns are blind. Still, they eventually grow big eyes for improved night vision as well.
+
+    The newborns need a lot of pampering and grooming by their mothers. Their eyes are round and adorable. They mostly feast on seeds, berries, mushrooms, acorns, moths, beetles, small birds, nuts, and small eggs. It is believed that some of the flying squirrels glow at night. They cannot fully fly but only glide and can make a 180-degree turn.
+    
+    We have learned that many animals are not permitted as a pet. Did you care to wonder why it is so? Why certain places are banning imported pets and making it illegal to own them? We shall dig in for a satisfactory explanation for the same.
+    
+    We must pay heed to the fact that every animal is not fit for humans to be used as a pet. Some animals love to be in the wild, on their own and they can get highly affected by separation from their surroundings and loved ones or forcefully importing them and keeping them as pets. We should see every living creature as ourselves and should have both sympathy and empathy toward them.
+    "}
+
+])
