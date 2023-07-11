cc68099d79a3e968e93aa7e597f8c696fc7a4be3
kiboma <omayioben@gmail.com>
2023-07-11 08:05:32 +0300

    Update routes to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  roots "animals#index"
+  root "animals#index"
   resources :animals
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
